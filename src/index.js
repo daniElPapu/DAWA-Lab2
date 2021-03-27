c0eae65d02b30c596e5fe7da08b2839d41aff874
daniElPapu <daniel.bedergal@tecsup.edu.pe>
2021-03-26 22:19:16 -0500

    Poniendo Partes en un array para mayor comodidad

--- src/index.js
+++ src/index.js
@@ -3,26 +3,26 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} 
-              part2={part2} 
-              part3={part3} />
-      <Total total={part1.exercises +part2.exercises+part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
@@ -34,9 +34,11 @@ const Header =(props)=>{
 const Content=(props)=>{
   return (
     <>
-      <Part part={props.part1.name} exercise={props.part1.exercises}/>
-      <Part part={props.part2.name} exercise={props.part2.exercises}/>
-      <Part part={props.part3.name} exercise={props.part3.exercises}/>
+      {props.parts.map((part) => {
+        return(
+          <Part part={part.name} exercise={part.exercises}/>
+        )
+      })}
     </>
   )
 }
@@ -49,7 +51,7 @@ const Part = (props)=>{
 }
 const Total = (props)=>{
   return(
-    <p>Number of exercises {props.total}</p>
+    <p>Number of exercises {props.parts.reduce((sum, part) => ( sum + part.exercises), 0)}</p>
   )
 }
 
